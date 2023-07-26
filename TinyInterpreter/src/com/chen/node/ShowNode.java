d1cb9036aa8bd434777e76c57f8894885a24194e
david <david@localhost>
2023-07-26 00:29:51 -0500

    show node

--- TinyInterpreter/src/com/chen/node/ShowNode.java
+++ TinyInterpreter/src/com/chen/node/ShowNode.java
@@ -3,6 +3,7 @@ package com.chen.node;
 import java.util.List;
 
 import com.chen.Token;
+import com.chen.TokenEnum;
 import com.chen.Tuple;
 
 public class ShowNode extends StmtNode {
@@ -11,7 +12,25 @@ public class ShowNode extends StmtNode {
 	
 	@Override
 	public Tuple makeNode(List<Token> list, int start) {
-		return null;
+		
+		if(list.get(start).getKind().equals(TokenEnum.ShowKeyword)) {
+			VarNode var = new VarNode();
+			Tuple result = var.makeNode(list, start + 1);
+			int end = result.getEnd();
+			
+			if(list.get(end).getKind().equals(TokenEnum.Semicolon)) {
+				return new Tuple(this, end + 1);
+			}
+
+			throw new RuntimeException("no semi");
+			
+			
+		}
+		else {
+			throw new RuntimeException("he a no show");
+		}
+		
+		
 	}
 }
 
